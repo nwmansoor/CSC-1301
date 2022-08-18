fdffe2f79605dbd960670a56b4ae2e881a1813c4
nwmansoor <105090588+nwmansoor@users.noreply.github.com>
2022-08-18 18:22:48 -0400

    Lab 1.2
    Printed "Hello" using a while loop

---
+++ PrintHello.java
@@ -0,0 +1,12 @@
+public class PrintHello{
+   public static void main(String[] args){
+      //Date: August 26th, 2021
+      //We have 3 sequential statements
+      int i = 10; //Sequential
+      while(i > 0){ //Iterative
+         System.out.println("Hello");
+         i = i -1; //decreases i by 1
+      }
+      System.out.println(i);
+   }
+}
